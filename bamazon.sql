fd0d645812f978202d4988ce5efd19aaf7bb4c41
Chris Minassian <cminassian@live.com>
2019-09-02 21:54:24 -0700

    I modified bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,11 +1,23 @@
 DROP DATABASE IF EXISTS bamazon_db;
-CREATE database bamazon_db;
+CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE products(
-  item_id INT NOT NULL AUTO_INCREMENT,
-  products_name VARCHAR(100),
-  department_name VARCHAR(100),
-  PRIMARY KEY (id)
+CREATE TABLE products (
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR (100) NOT NULL,
+department_name VARCHAR (20) NOT NULL,
+price DECIMAL (13,2) NOT NULL,
+stock_quantity INT (10) NOT NULL,
+PRIMARY KEY(item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+("13 Hours: The Secret Soldiers of Benghazi", "Movies", 14.99, 10),
+("LED Tall Floor Lamp", "Home Decor", 29.99, 10),
+("High Sierra Loop Backpack for Men", "Sports & Outdoors", 29.99, 10),
+("Hot Hand Poker Supply Folding Poker", "Sports & Outdoors", 276.66, 5),
+("Classic Premium All Purpose Clear Wine Glasses", "Sports & Outdoors", 18.50, 5), 
+("Coleman Gas Stoves", "Backpacking and Camping Stoves", 43.88, 3),
+("")
