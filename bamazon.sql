673e8b896d99c52ed13d034cff24ec673a3fc957
Chris Minassian <cminassian@live.com>
2019-09-06 21:57:14 -0700

    updated

---
+++ bamazon.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR (100) NOT NULL,
+department_name VARCHAR (20) NOT NULL,
+price DECIMAL (13,2) NOT NULL,
+stock_quantity INT (10) NOT NULL,
+PRIMARY KEY(item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+("13 Hours: The Secret Soldiers of Benghazi", "Movies", 14.99, 10),
+("LED Tall Floor Lamp", "Home Decor", 29.99, 10),
+("High Sierra Loop Backpack for Men", "Sports & Outdoors", 29.99, 10),
+("Hot Hand Poker Supply Folding Poker", "Sports & Outdoors", 276.66, 18),
+("Classic Premium All Purpose Clear Wine Glasses", "Sports & Outdoors", 18.50, 24), 
+("Coleman Gas Stoves", "Backpacking and Camping Stoves", 43.88, 33),
+("Hasbro Gaming Rubik's Cube", "Toys and Games", 4.49,90),
+("PANASONIC LUMIX GH5 4K Digital Camera, 20.3 Megapixel Mirrorless Camera with Digital Live", "Camera and Photo", 1497.99, 120),
+("Pampers Swaddlers Disposbale Baby Diapers", "Health & Personal Care", 32.38, 55),
+("Hatch Baby Rest Sound Machine Night Light", "Nursery Night Lights", 59.99, 140);
+
+
